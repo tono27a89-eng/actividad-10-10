77e1783e38bb64f66bbf891bac60647cb1314d42
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-04 16:16:36 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividad_10__10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cree un diccionario de inventario. Permita al usuario eliminar un producto ingresando su código.");
+
+
+            for (int i = 0; i < 3; i++)
+            {
+
+
+                Console.WriteLine($"Ingrese  ID NO.{i + 1} de 3 productos");
+                int.TryParse(Console.ReadLine(), out Id);
+                if (estudiante.ContainsKey(Id))
+                {
+                    Console.WriteLine("ID ya ingresado");
+                    i--;
+
+                }
+                else
+                {
+                    Console.WriteLine("ingrese nombre:");
+                    nombre = Console.ReadLine();
+                    estudiante.Add(Id, nombre);
+
+                }
+
+            }
+            bool validar = false;
+            while (validar != true)
+            {
+
+
+                Console.WriteLine("ingrese ID de estudiante que desea elimar");
+                int.TryParse(Console.ReadLine(), out Id);
+                if (estudiante.ContainsKey(Id))
+                {
+                    estudiante.Remove(Id);
+                    validar = true;
+                }
+                else
+                {
+                    Console.WriteLine("ID invalido");
+                }
+            }
+            foreach (var item in estudiante)
+            {
+                Console.WriteLine("prodcuto Id: " + item.Key + "valor: " + item.Value);
+
+            }
+
+        }
+    }
+}
